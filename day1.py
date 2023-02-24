0af8dbd68130e80f5d37075609c2394762416e0c
Gabriel Nunes <49696901+Ichedt@users.noreply.github.com>
2023-02-24 09:03:32 -0300

    Day 1 Project
    First project of the Python Bootcamp.

---
+++ day1.py
@@ -0,0 +1,19 @@
+# Day 1 - Band Name Generator
+#
+# tags: string, print, input, variables
+
+print("Welcome to the Band Name Generator!")
+
+print("Which city did you grow up in?")
+city = input()
+
+print("Choose a pet name.")
+pet = input()
+
+band_name = city + " " + pet + "s"
+print(f"Your band name is: {band_name}.")
+
+# Notes:
+#
+# Name convention: Google Python Style Guide suggests the following convention for naming things:
+# module_name, package_name, ClassName, method_name, ExceptionName, function_name, GLOBAL_CONSTANT_NAME, global_var_name, instance_var_name, function_parameter_name, local_var_name, CLASS_CONSTANT_NAME.
