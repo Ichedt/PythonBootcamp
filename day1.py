416913d6b9bf04e8899c71335f1c98dc6b34a4ef
Gabriel Nunes <49696901+Ichedt@users.noreply.github.com>
2023-02-26 20:44:12 -0300

    Update day1.py
    Changed format in the notes text.

--- day1.py
+++ day1.py
@@ -13,7 +13,6 @@ pet = input()
 band_name = city + " " + pet + "s"
 print(f"Your band name is: {band_name}.")
 
-# Notes:
-#
+# NOTES
 # Name convention: Google Python Style Guide suggests the following convention for naming things:
 # module_name, package_name, ClassName, method_name, ExceptionName, function_name, GLOBAL_CONSTANT_NAME, global_var_name, instance_var_name, function_parameter_name, local_var_name, CLASS_CONSTANT_NAME.
