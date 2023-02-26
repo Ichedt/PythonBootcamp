23068ddc8481e7a527af9b38e2153dc44a8f972d
Gabriel Nunes <49696901+Ichedt@users.noreply.github.com>
2023-02-26 11:48:12 -0300

    Day 4 Project

---
+++ day4.py
@@ -0,0 +1,36 @@
+# Day 3 - Rock Paper Scissors
+#
+# tags: randomisation, modules,  lists
+
+import random
+
+
+print("What do you choose? [R]ock, [P]aper or [S]cissors?")
+user_choice = input().lower()
+
+options = ["Rock", "Paper", "Scissors"]
+computer_choice = random.choice(options)
+
+if user_choice == computer_choice:
+    print("Draw!")
+
+elif user_choice == "r":
+    if computer_choice == "Paper":
+        print("Computer chose Paper, you lost!")
+    else:
+        print("Computer chose Scissors, you win!")
+
+elif user_choice == "p":
+    if computer_choice == "Rock":
+        print("Computer chose Rock, you win!")
+    else:
+        print("Computer chose Scissors, you lost!")
+
+elif user_choice == "s":
+    if computer_choice == "Rock":
+        print("Computer chose Rock, you lost!")
+    else:
+        print("Computer chose Paper, you win!")
+
+else:
+    print("Invalid option!")
