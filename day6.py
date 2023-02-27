04af00097fe8b42e348b64afc58a1db60fafe4d1
Gabriel Nunes <49696901+Ichedt@users.noreply.github.com>
2023-02-27 16:10:27 -0300

    Day 6 Project

---
+++ day6.py
@@ -0,0 +1,25 @@
+# Day 6 - Escaping The Maze
+# This project uses Reeborg's World to run.
+# Repository: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+#
+# tags: function, code block, while loop
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
