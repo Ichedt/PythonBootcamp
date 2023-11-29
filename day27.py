f3a2b9ecab22232dd4a14f8935b1d7a522b57b18
Gabriel Nunes <gabrielnunes9842@gmail.com>
2023-11-29 00:14:24 -0300

    Update Day 27

--- day27.py
+++ day27.py
@@ -1,5 +1,16 @@
 """
 Day 27 - Distance Converter
 
-tags: gui, tkinter, function arguments
+tags: graphical user interface, tkinter, function arguments
 """
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tkinter.Label(text="I'm a label", font=("Arial", 24, "normal"))
+my_label.pack()
+
+window.mainloop()
