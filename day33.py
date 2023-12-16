8fa70f6f0106974381fe37fe1d05e098ac255ff5
Gabriel Nunes <gabrielnunes9842@gmail.com>
2023-12-16 17:33:40 -0300

    Added Day 33

--- day33.py
+++ day33.py
@@ -3,3 +3,13 @@ Day 33 - ISS Tracker
 
 tags: API
 """
+import requests
+
+# Pylint recomends using 'timeout' parameter to avoid waiting for a long time
+# The timeout is in seconds
+response = requests.get(url="http://api.open-notify.org/iss-now.json", timeout=10)
+
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
