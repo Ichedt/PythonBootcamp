f08e06ddd1c7d23935778411441a3f3e2a944e76
Gabriel Nunes <gabrielnunes9842@gmail.com>
2024-01-13 13:10:12 -0300

    Finished Day 66

--- day66/main.py
+++ day66/main.py
@@ -56,7 +56,6 @@ def home():
 @app.route("/random", methods=["GET"])
 def get_random_cafe():
     """Return a random cafe in JSON form."""
-
     result = db.session.execute(db.select(Cafe))
     all_cafes = result.scalars().all()
     random_cafe = random.choice(all_cafes)
@@ -67,7 +66,6 @@ def get_random_cafe():
 @app.route("/all", methods=["GET"])
 def get_all_cafes():
     """Return all cafes in the database in JSON form."""
-
     result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
     all_cafes = result.scalars().all()
 
@@ -77,7 +75,6 @@ def get_all_cafes():
 @app.route("/search", methods=["GET"])
 def get_cafe_at_location():
     """Return all cafes at the location query in JSON form."""
-
     query_location = request.args.get("loc")
     result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
     # This might get more than one cafe per location
@@ -93,9 +90,82 @@ def get_cafe_at_location():
     )
 
 
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    """Add a new cafe to the database."""
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    return jsonify(response={"Success": "Successfully added new cafe."})
+
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    """Update the price of the selected cafe."""
+    new_price = request.args.get("new_price")
+    cafe = db.session.get(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        # Add the status code after the jsonify method. 200 = Ok
+        return jsonify(response={"Success": "Successfully updated the price."}), 200
+
+    # 404 = Not found
+    return (
+        jsonify(error={"Not found": "Sorry, this cafe was not found in the database."}),
+        404,
+    )
+
+
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api_key")
+    if api_key == "APIKey":  # Example API key
+        cafe = db.session.get(Cafe, cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return (
+                jsonify(
+                    response={"Success": "Successfully deleted cafe from database."}
+                ),
+                200,
+            )
+        else:
+            return (
+                jsonify(
+                    error={
+                        "Not found": "Sorry, this cafe was not found in the database."
+                    }
+                ),
+                404,
+            )
+
+    return (
+        jsonify(
+            error={
+                "Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."
+            }
+        ),
+        403,
+    )
+
+
 def main():
     """Run the main code."""
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 if __name__ == "__main__":
